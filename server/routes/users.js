cbeba1e64891cdf6b84462f8598fe86c1752a727
deepanshu-aggarwal <deepanshu.26.da@gmail.com>
2023-01-26 15:00:03 +0530

    added token verification and user routes setup

---
+++ server/routes/users.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+  getPosts,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const userRoutes = express.Router();
+
+/* READ */
+userRoutes.get("/:id", verifyToken, getUser);
+userRoutes.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+userRoutes.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default userRoutes;
